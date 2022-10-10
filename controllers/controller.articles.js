d61c36383e9fe7e90f199cdfa80d32ed26cfdec7
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2022-10-10 16:38:42 +0100

    completed ticket#4 GET api/articles/articleId

---
+++ controllers/controller.articles.js
@@ -0,0 +1,12 @@
+const { fetchArticleById } = require("../models/model.articles.js");
+
+exports.getArticle = (req, res, next) => {
+  const { article_id } = req.params;
+  fetchArticleById(article_id)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
