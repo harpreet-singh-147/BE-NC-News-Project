f65d1e5ab0aae362804b84d0dbaa32db667157b9
harpreet-singh-147 <h_singh_007@hotmail.co.uk>
2022-10-11 15:57:38 +0100

    ticket#7 added comment count property on article response object

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -46,23 +46,23 @@ describe("GET /api/articles/:article_id", () => {
         "body"
         "topic"
         "created_at"
-        "votes"`, () => {
+        "votes"
+        "comment_count"`, () => {
     return request(app)
       .get("/api/articles/1")
       .expect(200)
       .then(({ body }) => {
         const { article } = body;
-        expect(article).toEqual(
-          expect.objectContaining({
-            article_id: 1,
-            title: "Living in the shadow of a great man",
-            topic: "mitch",
-            author: "butter_bridge",
-            body: "I find this existence challenging",
-            created_at: "2020-07-09T20:11:00.000Z",
-            votes: 100,
-          })
-        );
+        expect(article).toEqual({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          comment_count: "11",
+        });
       });
   });
   test(`404: /api/articles/:article_id - if article_id is valid but it doesn't exist in database`, () => {
